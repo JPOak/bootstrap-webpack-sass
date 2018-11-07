ca9afc206df62ac75d0d00745e481d6c892ae5c2
JP Mac <josh@whosjosh.com>
2018-11-07 15:47:20 -0800

    Update to Webpack 4.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bootstrap 4, Webpack, and Sass Starter
+# Bootstrap 4, Webpack 4, and Sass Starter
 
 This is to help jump start a Bootstrap 4, Webpack, Sass project.
 
@@ -9,10 +9,10 @@ You will need updated versions of Node and NPM to get started.
 ### Building
 
 ```
-npm run build
+npm run dist
 ```
 
-Will compile scss and js with sourcemaps and copy from "src" to "dist." Images in assets will be optimized. Note: Depending on how many images you have it may take awhile on first run. demo.html example to make sure things look right.
+Will compile scss and js with sourcemaps and copy from "src" to "dist." demo.html example to make sure things look right.
 
 ```
 npm run start
@@ -21,12 +21,13 @@ npm run start
 Will launch browsersync and watch changes in scss and js in the "src" dirctory, and html files in the root directory.
 
 ```
-npm run production
+npm run build
 ```
 
-Will minify all the files and remove sourcemaps and copy to the "dist" directory.
+Will minify all the files and remove sourcemaps and copy to the "dist" directory. Images in assets will be optimized. Note: Depending on how many images you have it may take awhile on first run.
 
 ## Built With
 
 * [Bootstrap](https://getbootstrap.com/) - Front-end framework
+* [HTML5 Boilerplate](https://html5boilerplate.com/) - Template Kickstart
 * [Webpack](https://webpack.js.org/) - Asset bundling
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,101 @@
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
 
-//Set production flag
-const isProduction = (process.env.NODE_ENV === 'production');
+module.exports = function(env, argv) {
 
-const config = {
-	entry: ['./src/assets/scripts/main.js', './src/assets/styles/_main.scss'],
-	output: {
-		path: path.resolve(__dirname, 'dist/scripts'),
-		filename: 'main.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-			    test: /\.scss$/,
-			    use: ExtractTextPlugin.extract({
-				    use: [
-					    {
-							loader: 'css-loader', options: {
-								sourceMap: !isProduction,
-								minimize: isProduction
-			                } //Compiles Sass to CSS minifies and removes maps in production
-					    },
-					    {
-							loader: 'postcss-loader', options: {
-			                	sourceMap: !isProduction
-			                }
-						},
-					    {
-				    		loader: 'sass-loader',  options: {
-		                		sourceMap: !isProduction
-		                	} //Compiles Sass to CSS
-					    }
-					]
-				})
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	plugins: [
-	 	new CleanWebpackPlugin(['dist/scripts', 'dist/styles']),
-	 	new ExtractTextPlugin('../styles/main.css'),
-	 	new BrowserSyncPlugin({
-	 		files: ['./*.html', './*.htm'],
-	      	//browse to http://localhost:3000/ during development
-		    host: 'localhost',
-		    port: 3000,
-		    server: { baseDir: ['./'] }
-	    }),
-		new CopyWebpackPlugin([{
-		      from: path.resolve(__dirname, 'src/assets/images'),
-		      to: path.resolve(__dirname, 'dist/images')
-		    }]),
-	    new ImageminPlugin({
-      		disable: process.env.NODE_ENV !== 'production', // Disable during development
-      		test: /\.(jpe?g|png|gif|svg)$/i,
-      		cacheFolder: path.resolve(__dirname, '.cache'),
-      		//For more about image settings: https://github.com/Klathmon/imagemin-webpack-plugin
-      		pngquant: { quality: '90', speed: 4},
-        	jpegtran: { progressive: true },
-        	gifsicle: { optimizationLevel: 1 },
-			svgo: {}
-    	}),
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery'
-		})
-	]
+  return {
+    entry: ['./src/assets/scripts/main.js', './src/assets/styles/_main.scss'],
+    output: {
+        path: path.resolve(__dirname, 'dist/scripts'),
+        filename: 'main.js'
+    },
+    performance: {
+      hints: false
+    },
+    devtool: argv.mode === 'production' ? false : 'source-map',
+    optimization: {
+        minimizer: [
+        new UglifyJsPlugin({
+          parallel: true,
+          extractComments: true,
+          sourceMap: false
+        }),
+        new OptimizeCSSAssetsPlugin({})//Compiles Sass to CSS minifies and removes maps in production
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader, options: {
+                            }
+                        },
+                        {
+                            loader: 'css-loader', options: {
+                                sourceMap: argv.mode === 'production' ? false : true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader', options: {
+                                sourceMap: argv.mode === 'production' ? false : true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',  options: {
+                                sourceMap: argv.mode === 'production' ? false : true
+                            }
+                        }
+                    ]
+            }
+        ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(argv.mode === 'production' ? ['dist/scripts/*.map', 'dist/styles/*.map'] : ['dist/scripts/*.LICENSE'], {
+      }),
+      new MiniCssExtractPlugin({
+          filename: "../styles/[name].css"
+          //chunkFilename: "[id].css"
+      }),
+      new BrowserSyncPlugin({
+          files: ['./*.html', './*.htm'],
+          //browse to http://localhost:3000/ during development
+          host: 'localhost',
+          port: 3000,
+          server: { baseDir: ['./'] }
+      }),
+      new CopyWebpackPlugin([{
+          from: path.resolve(__dirname, 'src/assets/images'),
+          to: path.resolve(__dirname, 'dist/images')
+      }]),
+      new ImageminPlugin({
+          disable: argv.mode === 'production' ? false : true,// Disable during development
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          cacheFolder: path.resolve(__dirname, '.cache'),
+          //For more about image settings: https://github.com/Klathmon/imagemin-webpack-plugin
+          pngquant: { quality: '90', speed: 4},
+          jpegtran: { progressive: true },
+          gifsicle: { optimizationLevel: 1 },
+          svgo: {}
+      }),
+      new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery'
+      })
+    ]
+  };
+
 };//Config end
-
-//Minimize JS only in Production
-if (process.env.NODE_ENV === 'production') {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-        	comments: false
-        })
-    );
-}
-
-module.exports = config;
